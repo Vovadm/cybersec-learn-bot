93a6a53a7cbb5ca847316ff9b6e2a0965b0a7106
raild <vovadm@hotmail.com>
2025-12-23 22:21:38 +0300

    docker fix

--- src/app/config.py
+++ src/app/config.py
@@ -3,25 +3,11 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     token: str = ""
-    db_host: str = "localhost"
-    db_port: int = 3306
-    db_name: str = "cybersec"
-    db_user: str = "user"
-    db_password: str = "pass"
-
-    @property
-    def database_url(self) -> str:
-        return (
-            f"mysql+aiomysql://{self.db_user}:"
-            f"{self.db_password}@"
-            f"{self.db_host}:{self.db_port}/"
-            f"{self.db_name}"
-        )
+    database_url: str = ""
 
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
-        case_sensitive = False
 
 
 settings = Settings()
